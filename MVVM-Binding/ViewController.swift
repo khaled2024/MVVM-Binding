72272b00c4d0b87c84244236f4aaf1c3897033cc
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2022-09-11 11:41:15 +0200

    Initial Commit

---
+++ MVVM-Binding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-Binding
+//
+//  Created by KhaleD HuSsien on 11/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
